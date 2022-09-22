6081ae2731882c5a9a1d685a72796e9d2e2daed0
Remix Workspace <>
2022-09-22 15:07:02 +0300

    first commit

---
+++ HelloWorld.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+//derleyici versiyonu belirlendi
+pragma solidity 0.8.7;
+
+// sozlesmemize HelloOWorld adini veriyoruz
+contract HelloOWorld{
+    string public myString="hello world";
+
+} 
