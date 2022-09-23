a73117072d2807415c20e2418505606eededa59c
Remix Workspace <>
2022-09-23 17:41:32 +0300

    immutable commit

---
+++ Immutable.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract Immutable {
+
+
+    address public immutable owner;
+
+    constructor() {
+        owner = msg.sender;
+        }
+
+    uint public x;
+    function foo() external {
+        require(msg.sender == owner);
+        x += 1;
+    }
+}
