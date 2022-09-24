a20232111ec28d9b1b81880ca03ec434a84b2a55
Remix Workspace <>
2022-09-24 14:07:17 +0300

    Call Other Contracts commit

---
+++ Callee.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+// Bir contract diger bir contracti iki sekilde cagirabilir.
+
+// Bunun en kolay yolu, sadece onu aramaktır, A.foo(x, y, z).
+
+// Diger yolu da low-level call. Bu yontem tavsiye edilmez
+
+
+contract Callee {
+    uint public x;
+    uint public value;
+
+    function setX(uint _x) public returns (uint) {
+        x = _x;
+        return x;
+    }
+
+    function setXandSendEther(uint _x) public payable returns (uint, uint) {
+        x = _x;
+        value = msg.value;
+
+        return (x, value);
+    }
+}
+
+contract Caller {
+    function setX(Callee _callee, uint _x) public {
+        uint x = _callee.setX(_x);
+    }
+
+    function setXFromAddress(address _addr, uint _x) public {
+        Callee callee = Callee(_addr);
+        callee.setX(_x);
+    }
+
+    function setXandSendEther(Callee _callee, uint _x) public payable {
+        (uint x, uint value) = _callee.setXandSendEther{value: msg.value}(_x);
+    }
+}
