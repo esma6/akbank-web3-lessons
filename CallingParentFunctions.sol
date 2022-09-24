c49d6cb88dfe13679a2e70dd033916b841ca06c1
Remix Workspace <>
2022-09-24 12:53:42 +0300

    calling parent functions commit

---
+++ CallingParentFunctions.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+/* Inheritance tree
+   A
+ /  \
+B   C
+ \ /
+  D
+*/
+
+//Parent contracts doğrudan veya super keywords kullanılarak çağrılabilir.
+
+// super keywords kullanarak, tüm parent contracts cagrilir.
+
+contract A {
+
+    event Log(string message);
+
+    function foo() public virtual {
+        emit Log("A.foo called");
+    }
+
+    function bar() public virtual {
+        emit Log("A.bar called");
+    }
+}
+
+contract B is A {
+    function foo() public virtual override {
+        emit Log("B.foo called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("B.bar called");
+        super.bar();
+    }
+}
+
+contract C is A {
+    function foo() public virtual override {
+        emit Log("C.foo called");
+        A.foo();
+    }
+
+    function bar() public virtual override {
+        emit Log("C.bar called");
+        super.bar();
+    }
+}
+
+contract D is B, C {
+
+
+    function foo() public override(B, C) {
+        super.foo();
+    }
+
+    function bar() public override(B, C) {
+        super.bar();
+    }
+}
+
